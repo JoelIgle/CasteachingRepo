eeda1ec7dc345d8b873a685542ed479877122171
JoelIgle <joeliglesias@iesebre.com>
2024-03-13 17:44:52 +0100

    Video Laravel Blade Components i Layouts

--- casteaching/resources/views/layouts/casteaching.blade.php
+++ casteaching/resources/views/layouts/casteaching.blade.php
@@ -1,29 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
+    <!-- Styles -->
+    @livewireStyles
+</head>
+<body class="font-sans antialiased">
+<x-banner/>
 
-    TODO
-        <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
-            {{ $slot }}
-        </div>
+<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    @livewire('navigation-menu')
 
-        @livewireScripts
-    </body>
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-white dark:bg-gray-100 shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endif
+
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
+
+@livewireScripts
+</body>
 </html>
--- casteaching/resources/views/videos/show.blade.php
+++ casteaching/resources/views/videos/show.blade.php
@@ -1,15 +1,15 @@
 <x-casteaching-layout>
-    @if($video)
-        <div class="text-gray-800">
+{{--    @if($video)--}}
+        <div class="text-white">
         <h1>{{ $video->title }}</h1>
         <ul>
             <li>{{ $video->description }}</li>
             <li>{{ $video->published_at }}</li>
         </ul>
         </div>
-    @else
-        <p>Video not found.</p>
-    @endif
+{{--    @else--}}
+{{--        <p>Video not found.</p>--}}
+{{--    @endif--}}
 
 </x-casteaching-layout>
 
