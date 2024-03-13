9a5784413ec7cb46e94fc4f05fb871bbf4c0e2c5
JoelIgle <joeliglesias@iesebre.com>
2024-03-13 16:55:08 +0100

    Ara si se veu la bd, estava en fondo blanc :(

--- casteaching/resources/views/videos/show.blade.php
+++ casteaching/resources/views/videos/show.blade.php
@@ -1,13 +1,16 @@
 <x-casteaching-layout>
     @if($video)
+        <div class="text-gray-800">
         <h1>{{ $video->title }}</h1>
         <ul>
             <li>{{ $video->description }}</li>
             <li>{{ $video->published_at }}</li>
         </ul>
+        </div>
     @else
         <p>Video not found.</p>
     @endif
+
 </x-casteaching-layout>
 
 
